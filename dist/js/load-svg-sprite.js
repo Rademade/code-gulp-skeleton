56268fc5c1639275f86bb511a63ef2a5f6e2767a
Maksym Shutiak <shutyak.maxym@gmail.com>
2019-12-24 15:30:26 +0200

    Update skeleton

--- dist/js/load-svg-sprite.js
+++
@@ -1,9 +0,0 @@
-const spriteFile = new XMLHttpRequest();
-spriteFile.open("GET", "./sprite.svg", true);
-spriteFile.onreadystatechange = function () {
-  const spriteReadyState = spriteFile.readyState === 4 && spriteFile.status === 200;
-  if (spriteReadyState) {
-    document.getElementById('svg-sprite').innerHTML = spriteFile.responseText;
-  }
-};
-spriteFile.send(null);
